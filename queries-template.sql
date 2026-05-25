9235bd01685acab80b15d898e2b05090d365ba2b
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-26 00:33:57 +0200

    change title to titulo in servicios table
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- queries-template.sql
+++ queries-template.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS servicios;
 
 CREATE TABLE IF NOT EXISTS servicios (
   id serial NOT NULL PRIMARY KEY,
-  title varchar(100) NOT NULL,
+  titulo varchar(100) NOT NULL,
   descripcion text NOT NULL,
   fecha date DEFAULT CURRENT_DATE,
   categoria varchar(15)
 );
 
-INSERT INTO servicios (title, descripcion, categoria)
+INSERT INTO servicios (titulo, descripcion, categoria)
 VALUES
   ('Servicio 1', 'Descripción del servicio 1', 'Asistencia'),
   ('Servicio 2', 'Descripción del servicio 2', 'Entretenimiento'),
