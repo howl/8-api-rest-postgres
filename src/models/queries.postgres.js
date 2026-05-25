c7e0334296b5f08754ce8e6bb64bfbe0cdcbe186
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-26 00:40:14 +0200

    add postgres queries
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

---
+++ src/models/queries.postgres.js
@@ -0,0 +1,9 @@
+const queries = {
+  getServices: 'SELECT * FROM servicios;',
+  getServiceById: 'SELECT * FROM servicios WHERE id = $1;',
+  addService: 'INSERT INTO servicios (titulo, descripcion, categoria) VALUES ($1, $2, $3);',
+  updateServiceById: 'UPDATE servicios SET titulo = $2, descripcion = $3, categoria = $4 WHERE id = $1;',
+  deleteServiceById: 'DELETE FROM servicios WHERE id = $1;'
+};
+
+module.exports = queries;
